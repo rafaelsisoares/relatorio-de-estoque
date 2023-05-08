224c736bb036255a960b0cf60b27a1bf209e0083
Rafael Soares <r.soares2015@outlook.com>
2023-05-08 15:51:57 -0300

    test: Implementa o teste do retorno da classe Product

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,29 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
+
+product_id = 1
+company = 'Organizações Tabajara'
+product_name = 'Faca Guincho'
+manufacturing_date = '2023-05-02'
+expiration_date = '2026-05-02'
+storage_info = 'em temperatura ambiente longe de fontes de umidade'
+serial = '12345678'
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        product_id,
+        product_name,
+        company,
+        manufacturing_date,
+        expiration_date,
+        serial,
+        storage_info
+    )
+
+    assert product.__repr__() == (
+        "O produto Faca Guincho "
+        "fabricado em 2023-05-02 "
+        "por Organizações Tabajara com validade "
+        "até 2026-05-02 "
+        "precisa ser armazenado em temperatura ambiente longe de fontes de umidade.")
