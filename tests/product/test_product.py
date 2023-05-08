a39f2fa2cef993bffe99bc709e706dbf6d836a74
Rafael Soares <r.soares2015@outlook.com>
2023-05-08 15:06:37 -0300

    test: Implementa o teste da classe Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,29 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
+product_id = 1
+company = 'Organizações Tabajara'
+product_name = 'Faca Guincho'
+manufacturing_date = '2023-05-02'
+expiration_date = '2026-05-02'
+storage_info = 'Armazenar em temperatura ambiente longe de fontes de umidade.'
+serial = '12345678'
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        product_id,
+        product_name,
+        company,
+        manufacturing_date,
+        expiration_date,
+        serial,
+        storage_info
+    )
+
+    assert product.id == product_id
+    assert product.nome_do_produto == product_name
+    assert product.nome_da_empresa == company
+    assert product.data_de_fabricacao == manufacturing_date
+    assert product.data_de_validade == expiration_date
+    assert product.instrucoes_de_armazenamento == storage_info
+    assert product.numero_de_serie == serial
