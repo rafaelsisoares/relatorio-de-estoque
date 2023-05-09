9a958294ff194766eab9258b4c2277c3c235ba13
Rafael Soares <r.soares2015@outlook.com>
2023-05-09 09:43:56 -0300

    feat: Implementa a classe CompleteReport

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,15 @@
-class CompleteReport:
-    pass
+from collections import Counter
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(self, products):
+        companies_stock = Counter([
+            product['nome_da_empresa'] for product in products
+            ]).most_common()
+        report = super().generate(products)
+        report += '\nProdutos estocados por empresa:\n'
+        for company, stock in companies_stock:
+            report += f"- {company}: {stock}\n"
+        return report
