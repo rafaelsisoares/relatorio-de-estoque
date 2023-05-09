9cc27d752630db12986e6a350ad256ff293f3049
Rafael Soares <r.soares2015@outlook.com>
2023-05-09 10:26:10 -0300

    feat: Implementa a classe Inventory e o metodo para leitura de .csv

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,36 @@
+import csv
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
+class Inventory:
+    @staticmethod
+    def csv_reader(path):
+        with open(path, mode="r", encoding="utf-8") as file:
+            data = csv.DictReader(file, delimiter=",")
+            return list(data)
+
+    @staticmethod
+    def json_reader(path):
+        raise NotImplementedError
+
+    @staticmethod
+    def xml_reader(path):
+        raise NotImplementedError
+
+    @staticmethod
+    def import_data(path, type):
+        ext = path.split(".")[-1]
+        if ext == "csv":
+            imported_data = Inventory.csv_reader(path)
+        elif ext == "json":
+            imported_data = Inventory.json_reader(path)
+        elif ext == "xml":
+            imported_data = Inventory.xml_reader(path)
+        else:
+            raise TypeError("Arquivo com formato inválido")
+
+        if type == "simples":
+            return SimpleReport.generate(imported_data)
+        else:
+            return CompleteReport.generate(imported_data)
