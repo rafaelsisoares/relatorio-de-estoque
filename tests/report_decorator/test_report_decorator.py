00fcdae83bc115db539ec47df20985561be3844b
Rafael Soares <r.soares2015@outlook.com>
2023-05-09 17:49:36 -0300

    test: Implementa teste da classe ColoredReport

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,30 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
+
+GREEN = '\033[32m'
+BLUE = '\033[36m'
+RED = '\033[31m'
+END = '\033[0m'
+PRODUCTS = [
+        {
+            "id": 1,
+            "nome_do_produto": "CADEIRA",
+            "nome_da_empresa": "Forces of Nature",
+            "data_de_fabricacao": "2022-04-04",
+            "data_de_validade": "2023-12-09",
+            "numero_de_serie": "FR48",
+            "instrucoes_de_armazenamento": "Conservar em local fresco",
+        }
+    ]
 
 
 def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+    phrases = [
+        f"{GREEN}Data de fabricação mais antiga:{END} {BLUE}2022-04-04{END}",
+        f"{GREEN}Data de validade mais próxima:{END} {BLUE}2023-12-09{END}",
+        f"{GREEN}Empresa com mais produtos:{END} {RED}Forces of Nature{END}"
+    ]
+    report = ColoredReport(SimpleReport).generate(PRODUCTS)
+    print(report)
+    for phrase in phrases:
+        assert phrase in report
