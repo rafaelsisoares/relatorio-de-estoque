34ba01c418bdd293ae260837c4dbe1dff9d83ee3
Rafael Soares <r.soares2015@outlook.com>
2023-06-27 23:20:07 -0300

    feat: Implementa a funcao main

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,3 +1,19 @@
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+import sys
+
+
 def main():
-    pass
-# initial commit
+    args = sys.argv
+    if args[1].endswith('.csv'):
+        inventory = InventoryRefactor(CsvImporter)
+    elif args[1].endswith('.json'):
+        inventory = InventoryRefactor(JsonImporter)
+    else:
+        inventory = InventoryRefactor(XmlImporter)
+    if len(args) != 3:
+        sys.stderr.write('Verifique os argumentos\n')
+    else:
+        sys.stdout.write(inventory.import_data(args[1], args[2]))
